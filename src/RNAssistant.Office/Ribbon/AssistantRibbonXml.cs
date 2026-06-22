3e2f82bec3cee47fad09f3fd242961a7319b8cfc
Sergey Petrov <sergeypetrovmtgrey@gmail.com>
2026-06-22 15:53:07 +0300

    Simplify Office ribbon

--- src/RNAssistant.Office/Ribbon/AssistantRibbonXml.cs
+++ src/RNAssistant.Office/Ribbon/AssistantRibbonXml.cs
@@ -9,16 +9,8 @@ namespace RNAssistant.Office.Ribbon
   <ribbon>
     <tabs>
       <tab id=""rnAssistantTab"" label=""RN Assistant"">
-        <group id=""rnAssistantMain"" label=""Assistant"">
+        <group id=""rnAssistantMain"" label=""Agent"">
           <button id=""openAssistant"" label=""Open Assistant"" size=""large"" imageMso=""HappyFace"" onAction=""OpenAssistant"" />
-          <button id=""summarize"" label=""Summarize"" imageMso=""ReviewNewComment"" onAction=""Summarize"" />
-          <button id=""explainSelection"" label=""Explain Selection"" imageMso=""ResearchPane"" onAction=""ExplainSelection"" />
-          <button id=""draftRewrite"" label=""Draft / Rewrite"" imageMso=""CreateMailRule"" onAction=""DraftRewrite"" />
-          <button id=""runSkill"" label=""Run Skill"" imageMso=""MacroPlay"" onAction=""RunSkill"" />
-        </group>
-        <group id=""rnAssistantManage"" label=""Manage"">
-          <button id=""settings"" label=""Settings"" imageMso=""AdpPrimaryKey"" onAction=""OpenSettings"" />
-          <button id=""context"" label=""Context"" imageMso=""FileDocumentInspect"" onAction=""OpenContext"" />
         </group>
       </tab>
     </tabs>
@@ -27,4 +19,3 @@ namespace RNAssistant.Office.Ribbon
         }
     }
 }
-
--- src/RNAssistant.PowerPointAddIn/AssistantRibbon.cs
+++ src/RNAssistant.PowerPointAddIn/AssistantRibbon.cs
@@ -21,12 +21,5 @@ namespace RNAssistant.PowerPointAddIn
 
         public void OnRibbonLoad(IRibbonUI ribbon) { }
         public void OpenAssistant(IRibbonControl control) { _addIn.ShowAssistant(); }
-        public void Summarize(IRibbonControl control) { _addIn.ShowAssistant("summarize"); }
-        public void ExplainSelection(IRibbonControl control) { _addIn.ShowAssistant("explain-selection"); }
-        public void DraftRewrite(IRibbonControl control) { _addIn.ShowAssistant("draft-rewrite"); }
-        public void RunSkill(IRibbonControl control) { _addIn.ShowAssistant("run-skill"); }
-        public void OpenSettings(IRibbonControl control) { _addIn.ShowAssistant("settings"); }
-        public void OpenContext(IRibbonControl control) { _addIn.ShowAssistant("context"); }
     }
 }
-
